486db5bd19abc39ee4a30ccebef73d14264202af
İpek Nur Günümdoğdu <90203389+ladyvahsi@users.noreply.github.com>
2023-12-28 12:05:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 //OOP: Nesne Tabanlı Programlama
 //Object
 
+
 function Soru(soruMetni, cevapSecenekleri, dogruCevap) {
     this.soruMetni = soruMetni;
     this.cevapSecenekleri = cevapSecenekleri;
     this.dogruCevap = dogruCevap;
-
-    this.cevabiKontrolEt = function (verilenCevap) {
-        return this.dogruCevap === verilenCevap;
-    }
 }
 
+Soru.prototype.cevabiKontrolEt = function(cevap) {
+    return cevap === this.dogruCevap
+}
 
 
 let sorular = [
@@ -20,9 +20,9 @@ let sorular = [
     new Soru ("JavaScript'te 'hoisting' kavramı nedir??" , { a:"Fonksiyonların yukarı taşınmasıdır.", b:"Değişkenlerin tanımlanmasının önceki bir aşamada gerçekleşmesidir.", c:"JavaScript'te kullanılan bir programlama dilidir." }, "b")
 ]
 
-function Quiz (sorular) {
+function Quiz(sorular) {
     this.sorular = sorular;
-    this.soruIndex = 0; 
+    this.soruIndex = 0;
 }
 
 Quiz.prototype.soruGetir = function() {
@@ -30,76 +30,64 @@ Quiz.prototype.soruGetir = function() {
 }
 
 const quiz = new Quiz(sorular);
-document.querySelector(".btn-start").addEventListener("click", function(){
+
+document.querySelector(".btn-start").addEventListener("click", function() {
     document.querySelector(".quiz_box").classList.add("active");
-    soruGoster(quiz.soruGetir())
+    soruGoster(quiz.soruGetir());
     document.querySelector(".next_btn").classList.remove("show");
-
 })
-
 
 document.querySelector(".next_btn").addEventListener("click", function() {
-    if (quiz.sorular.length != quiz.soruIndex +1) {
-        soruGoster(quiz.soruGetir())
-        document.querySelector(".next_btn").classList.remove("show");
+    if (quiz.sorular.length != quiz.soruIndex + 1) {
         quiz.soruIndex += 1;
-        } else {
-            console.log("quiz bitti");
-        }
-})
+        soruGoster(quiz.soruGetir());
+        document.querySelector(".next_btn").classList.remove("show");
+    } else {
+        console.log("quiz bitti");
+    }
+});
 
+const option_list = document.querySelector(".option_list");
+const correctIcon = '<div class="icon"><i class="fas fa-check"></i></div>';
+const incorrectIcon = '<div class="icon"><i class="fas fa-times"></i></div>';
 
 function soruGoster(soru) {
     let question = `<span>${soru.soruMetni}</span>`;
     let options = '';
 
-    for(let cevap in soru.cevapSecenekleri){
-        options +=
-        `
-            <div class="option">
-                <span><b>${cevap}</b>: ${soru.cevapSecenekleri[cevap]}</span>
-            </div>
-        
-        `;
+    for(let cevap in soru.cevapSecenekleri) {
+        options += 
+            `
+                <div class="option"> 
+                    <span><b>${cevap}</b>: ${soru.cevapSecenekleri[cevap]}</span>
+                </div>
+            `;
     }
-
-    const option_list = document.querySelector(".option_list");
-    const correctIcon = '<div class="icon"><i class="fas fa-check"></i></div>';
-    const incorrectIcon = '<div class="icon"><i class="fas fa-times"></i></div>';
-
     document.querySelector(".question_text").innerHTML = question;
     option_list.innerHTML = options;
 
     const option = option_list.querySelectorAll(".option");
 
-    for (let opt of option) {
+    for(let opt of option) {
         opt.setAttribute("onclick", "optionSelected(this)")
     }
 }
-
-const correctIcon = '<div class="icon"><i class="fas fa-check"></i></div>';
-const incorrectIcon = '<div class="icon"><i class="fas fa-times"></i></div>';
 
 function optionSelected(option) {
     let cevap = option.querySelector("span b").textContent;
     let soru = quiz.soruGetir();
 
-    if (soru.cevabiKontrolEt(cevap)) {
+    if(soru.cevabiKontrolEt(cevap)) {
         option.classList.add("correct");
         option.insertAdjacentHTML("beforeend", correctIcon);
     } else {
         option.classList.add("incorrect");
         option.insertAdjacentHTML("beforeend", incorrectIcon);
-
     }
 
-    const option_list = document.querySelector(".option_list");
-    const optionArray = Array.from(option_list.children);
-
-    for (let i = 0; i < optionArray.length; i++) {
-        optionArray[i].classList.add("disabled");
+    for(let i=0; i < option_list.children.length; i++) {
+        option_list.children[i].classList.add("disabled");
     }
 
     document.querySelector(".next_btn").classList.add("show");
-
 }
